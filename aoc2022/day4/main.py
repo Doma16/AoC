c704d33be690f3d70abd313a307215cc70d76190
domaSaric <ds53234@fer.hr>
2022-12-04 19:59:01 +0100

    day 4

--- aoc2022/day4/main.py
+++ aoc2022/day4/main.py
@@ -1,6 +1,30 @@
 import numpy as np
+import os 
 
-with open('./aoc2022/day4/input.txt','r') as file:
+print(os.getcwd())
+
+with open('../adventOfCode/aoc2022/day4/task.txt','r') as file:
     inputs = [x.strip() for x in file]
 
-print(inputs)
+sections = inputs
+count = 0
+
+for section in sections:
+    r1, r2 = section.split(",")
+    elf1 = []
+    elf2 = []
+    numbs1 = r1.split("-")
+    numbs2 = r2.split("-")
+    for i in range(int(numbs1[0]),int(numbs1[1])+1):
+        elf1.append(i)
+    for i in range(int(numbs2[0]), int(numbs2[1])+1):
+        elf2.append(i)
+    
+    s = set()
+    for e in elf1:
+        if e in elf2:
+            s.add(e)
+    
+    if len(s) > 0:
+        count += 1
+print(count)
