5083b131a968e309df7036fce3a2913bec3ebea5
domaSaric <ds53234@fer.hr>
2022-12-06 06:40:23 +0100

    day6

--- aoc2022/day6/main.py
+++ aoc2022/day6/main.py
@@ -1,6 +1,22 @@
 import numpy as np
 
-with open('./aoc2022/day6/input.txt','r') as file:
+with open('./aoc2022/day6/task.txt','r') as file:
     inputs = [x.strip() for x in file] 
 
-print(inputs)
+buffer = inputs[0]
+
+l = list(buffer)
+
+i = 0
+length = 14
+while( i < (len(buffer)-3)):
+    ll = []
+    for j in range(length):
+        ll.append(buffer[i+j])
+
+    s = { e for e in ll }
+    if len(s) == length:
+        break
+    i+=1
+    
+print(i+length)
