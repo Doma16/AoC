7a11b23f84c99d154c7a2936ee1b72cbbfd9952c
domaSaric <ds53234@fer.hr>
2022-12-07 17:21:28 +0100

    day 7 and init day 8

--- aoc2022/day7/main.py
+++ aoc2022/day7/main.py
@@ -1,14 +1,14 @@
 import numpy as np
 
-with open('./aoc2022/day7/input.txt','r') as file:
+with open('./aoc2022/day7/task.txt','r') as file:
     inputs = [ x.strip() for x in file ]
 
 #print(inputs)
-
-filesystem = [['/']]
-prev = None
+            # 0    # 1    #  2    #  3
+            #name, #dirs, #files, #previous
+filesystem = ['/', list(), list(), None]
 current = filesystem
-root = filesystem[0]
+dirs = []
 
 pos = 0
 n = len(inputs)
@@ -21,26 +21,68 @@ while(pos < n):
                 pos+=1
                 file = inputs[pos]
                 if file[:3] == 'dir':
-                    current.append([file[3:].strip()])
+                    newDir = [file[3:].strip(), list(), list(), current]
+                    current[1].append(newDir)
+                    dirs.append(newDir)
                 else:
                     size, name = file.split(" ")
                     size = int(size.strip())
                     name = name.strip()
-                    current.append((size,name))
+                    current[2].append((size,name))
         else:
             # cd
             name = command[2:].strip()
             if name == "..":
-                current = prev
+                current = current[3]
+            elif name == "/":
+                current = filesystem
             else:
-                for e in current:
-                    if e.__class__ == list:
-                        if e[0] == name:
-                            prev = current
-                            current = e
+                for e in current[1]:
+                    if e[0] == name:
+                        current = e
     else:
         #not command
         size,name = 1,1
     pos +=1
 
-print(filesystem)
+#print(filesystem)
+#done filesystem - weird implementation
+
+def sumDIR(l):
+    sum = 0
+    for f in l[2]:
+        sum += f[0]
+    for dirs in l[1]:
+        sum += sumDIR(dirs)
+    return sum
+
+#print(sumDIR(filesystem))
+#working sum dir function
+#now need to iterate over dirs
+
+max_size = 1e5
+size = 0
+for dir in dirs:
+    val = sumDIR(dir)
+    if val < max_size:
+        size += val
+
+
+print(size)
+
+#part 2
+
+disk_space = 70000000
+unused = 30000000
+current_allocation = sumDIR(filesystem)
+
+diff = disk_space - current_allocation
+
+minimal = current_allocation
+for dir in dirs:
+    deldiff = sumDIR(dir)
+    if current_allocation - deldiff < disk_space - unused:
+        if deldiff < minimal:
+            minimal = deldiff
+
+print(minimal)   
